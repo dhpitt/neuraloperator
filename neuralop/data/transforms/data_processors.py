3086f6fef070f04fc2965dd3b7b80adf763dedb8
dhpitt <david.h.pitt@gmail.com>
2024-06-24 14:10:41 -0700

    add gino subsampling data processor

--- neuralop/data/transforms/data_processors.py
+++ neuralop/data/transforms/data_processors.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+import random
 
 import torch
 from neuralop.training.patching import MultigridPatching2D
@@ -365,4 +366,88 @@ class MGPatchingDataProcessor(DataProcessor):
         data_dict = self.preprocess(data_dict)
         output = self.model(**data_dict)
         output, data_dict = self.postprocess(output, data_dict)
+        return output, data_dict
+
+class SubsamplingGINODataProcessor(DataProcessor):
+    """SubsamplingGINODataProcessor does the exact same thing
+    as a DefaultDataProcessor with the addition of randomly subsampling
+    points in the model's domain and codomain. Written specifically
+    for the forward call signature of neuralop.models.GINO
+    """
+    def __init__(
+        self, in_normalizer=None, out_normalizer=None, positional_encoding=None, subsampling_level=None
+    ):
+        """A simple processor to pre/post process data before training/inferencing a model.
+
+        Parameters
+        ----------
+        in_normalizer : Transform, optional, default is None
+            normalizer (e.g. StandardScaler) for the input samples
+        out_normalizer : Transform, optional, default is None
+            normalizer (e.g. StandardScaler) for the target and predicted samples
+        positional_encoding : Processor, optional, default is None
+            class that appends a positional encoding to the input
+        subsampling_level : float, optional, default is None
+            level at which to subsample points
+        """
+        super().__init__()
+        self.in_normalizer = in_normalizer
+        self.out_normalizer = out_normalizer
+        self.positional_encoding = positional_encoding
+        self.subsampling_level = subsampling_level
+        self.device = "cpu"
+
+    def wrap(self, model):
+        self.model = model
+        return self
+
+    def to(self, device):
+        if self.in_normalizer is not None:
+            self.in_normalizer = self.in_normalizer.to(device)
+        if self.out_normalizer is not None:
+            self.out_normalizer = self.out_normalizer.to(device)
+        self.device = device
+        return self
+
+    def preprocess (self, data_dict, batched=True):
+        # inputs of shape (_, n_in, in_dim)
+        x = data_dict["x"].to(self.device)
+        input_geom = data_dict["input_geom"].to(self.device)
+        n_in = int(input_geom.shape[1] * self.subsampling_level)
+        input_indices = random.sample(list(range(input_geom.shape[0])), k=n_in)
+        x = x[:, input_indices, ...]
+        input_geom = input_geom[:, input_indices, ...]
+
+        # outputs of shape (_, n_out, out_dim)
+        output_queries = data_dict["output_queries"].to(self.device)
+        y = data_dict["y"].to(self.device)
+        n_out = int(output_queries.shape[1] * self.subsampling_level)
+        output_indices = random.sample(list(range(output_queries.shape[0])), k=n_out)
+        y = y[:, output_indices, ...]
+        output_queries = output_queries[:, output_indices, ...]
+
+        if self.in_normalizer is not None:
+            x = self.in_normalizer.transform(x)
+        if self.positional_encoding is not None:
+            x = self.positional_encoding(x, batched=batched)
+        if self.out_normalizer is not None and self.train:
+            y = self.out_normalizer.transform(y)
+
+        data_dict["x"] = x
+        data_dict["y"] = y
+
+        return data_dict
+
+    def postprocess(self, output, data_dict):
+        y = data_dict["y"]
+        if self.out_normalizer and not self.train:
+            output = self.out_normalizer.inverse_transform(output)
+            y = self.out_normalizer.inverse_transform(y)
+        data_dict["y"] = y
+        return output, data_dict
+
+    def forward(self, **data_dict):
+        data_dict = self.preprocess(data_dict)
+        output = self.model(data_dict["x"])
+        output = self.postprocess(output)
         return output, data_dict
